26db7c4c3f77f0c457f76aaa54f8fbe55015f61d
Sina <malekikh@sheridancollege.ca>
2019-03-26 11:13:22 -0400

    This is Sina committing this project to GitHub for Java 2 Group Project.

---
+++ src/OPbd_User_Interface/OPbd_Management_System.java
@@ -0,0 +1,33 @@
+package OPbd_User_Interface;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author SinaM
+ */
+public class OPbd_Management_System extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        String css = OPbd_Management_System.class.getResource("OPbd.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        
+        stage.setScene(scene);
+        stage.setTitle("OPbd Management System");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
