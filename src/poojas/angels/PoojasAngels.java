a462b9e1c31f23824bdfeadbf492e47c71a20b0a
saeedbas-bassam <saeedbas-bassam@10.16.25.250>
2019-03-30 14:52:59 -0400

    Set up ability to write to and read from file.

--- src/poojas/angels/PoojasAngels.java
+++ src/poojas/angels/PoojasAngels.java
@@ -1,6 +1,8 @@
 package poojas.angels;
 
-import javafx.application.*;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 /**
  * @author Bassam
@@ -11,12 +13,43 @@ public class PoojasAngels {
      *
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Company ivansProCompany = new Company("Ivans Company");
+    public static void main(String[] args) throws Exception {
 
-        System.out.println(ivansProCompany);
+        String fileName = "database\\videogamelist.txt";
+        /*
+        Company company = new Company("Blizzard");
         
+        VideoGame game = new VideoGame("Overwatch",
+                "fps",
+                2016,
+                9.9,
+                ESRB.TEEN,
+                new Platform[]{Platform.WINDOWS, Platform.PS4, Platform.XBOX_ONE},
+                new Company[]{company},
+                new Company[]{company});
         
+        FileOutputStream fo = new FileOutputStream(fileName);
+        ObjectOutputStream oo = new ObjectOutputStream(fo);
+        
+        oo.writeObject(game);
+        
+        oo.close();
+        fo.close();
+         */
+        FileInputStream fi = new FileInputStream(fileName);
+        ObjectInputStream oi = new ObjectInputStream(fi);
+
+        while (true) {
+            try {
+                VideoGame games = (VideoGame) oi.readObject();
+                System.out.println(games);
+            } catch (EOFException e) {
+                System.out.println("Reached End of File.");
+                break;
+            }
+        }
+
+        fi.close();
+        oi.close();
     }
-
 }
