dab3a911180dbd19b1cdcb2c52b9a1b3208f040f
Sina <malekikh@sheridancollege.ca>
2019-04-09 02:12:48 -0400

    Sina is committing this project at 2:09am April 9. UI is 100% done with some events.

---
+++ src/poojas/angels/Company.java
@@ -0,0 +1,79 @@
+package poojas.angels;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Company implements Serializable {
+
+    private String name;
+    private ArrayList<VideoGame> published;
+    private ArrayList<VideoGame> developed;
+
+    public Company() {
+        this.published = new ArrayList<>();
+        this.developed = new ArrayList<>();
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public Company(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @param name
+     * @param published
+     * @param developed
+     */
+    public Company(String name, VideoGame[] published, VideoGame[] developed) {
+        this.name = name;
+        this.published = new ArrayList<>(Arrays.asList(published));
+        this.developed = new ArrayList<>(Arrays.asList(developed));
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<VideoGame> getPublished() {
+        return this.published;
+    }
+
+    /**
+     *
+     * @param published
+     */
+    public void setPublished(ArrayList<VideoGame> published) {
+        this.published = published;
+    }
+
+    public ArrayList<VideoGame> getDeveloped() {
+        return this.developed;
+    }
+
+    /**
+     *
+     * @param developed
+     */
+    public void setDeveloped(ArrayList<VideoGame> developed) {
+        this.developed = developed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s Published: %s, Developed: %s", this.name, this.published, this.developed);
+    }
+
+}
---
+++ src/poojas/angels/VideoGameList.java
@@ -0,0 +1,70 @@
+package poojas.angels;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class VideoGameList {
+
+    private ArrayList<VideoGame> games;
+
+    public VideoGameList() {
+        this.games = new ArrayList<>();
+    }
+
+    /**
+     *
+     * @param games
+     */
+    public VideoGameList(VideoGame[] games) {
+        this.games = new ArrayList<>(Arrays.asList(games));
+    }
+
+    public ArrayList<VideoGame> getGames() {
+        return this.games;
+    }
+
+    /**
+     *
+     * @param games
+     */
+    public void setGames(ArrayList<VideoGame> games) {
+        this.games = games;
+    }
+
+    /**
+     *
+     * @param game
+     */
+    public void addGame(VideoGame game) {
+        this.games.add(game);
+    }
+
+    /**
+     *
+     * @param game
+     */
+    public void removeGame(VideoGame game) {
+        this.games.remove(game);
+    }
+
+    public void sort() {
+        // TODO - implement VideoGameList.sort
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * A single line list of all Video Games
+     * @return String representation
+     */
+    @Override
+    public String toString() {
+        String gameList = "";
+
+        for (VideoGame game : this.games) {
+            gameList += game + " ";
+        }
+
+        return gameList.trim();
+    }
+
+}
